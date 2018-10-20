bc8ea1aa561fd93e982556b0eb8c60f06960ca45
ghblw <1147407739@qq.com>
2018-10-20 16:16:38 +0800

    commit

--- First_three_strings/Find.sh
+++ First_three_strings/Find.sh
@@ -7,7 +7,7 @@ num=("" "" "")
 
 function find_words() {
     #查询文本，更新字符串,长度，行号, 路径
-    words=`cat $1 | tr -s -c "a-zA-Z" "\n"`    #那个strings会把注释那段/*解析
+    words=`cat $1 | tr -s -c "a-zA-Z0-9" "\n"`    #那个strings会把注释那段/*解析
     for i in $words; do
         len=${#i}    #字符串长度
         line_num=`grep -n $i $1 | head -n 1 | cut -d ":" -f 1`    #获取字符串所在文本行号
@@ -32,11 +32,11 @@ function find_words() {
             max_str[1]=$i
             path[1]=$1
             num[1]=$line_num
-        elif [[ $len -gt ${length[0]} ]]; then
-            length[0]=$len
-            max_str[0]=$i
-            path[0]=$1
-            num[0]=$line_num
+        elif [[ $len -gt ${length[2]} ]]; then
+            length[2]=$len
+            max_str[2]=$i
+            path[2]=$1
+            num[2]=$line_num
         fi
     done;
 }
@@ -44,11 +44,10 @@ function find_words() {
 function filter() {    #判断文件是否为特殊类型文件进行过滤
     a=(o rmvb png img jpg jpeg gif md avi zip tar gz 7z)
     suffix=`echo $1 | awk -F. '{print $NF}'`    #提取后缀
-    echo $suffix
     echo ${a[*]} | grep "$suffix" > /dev/null
 
     if [[ $? -eq 0 ]]; then
-        echo $1"需要过滤"
+        #echo $1"需要过滤"
         return 1    #需要过滤
     fi
     return 0    #不需要过滤
@@ -59,26 +58,26 @@ function Exec() {
     if [[ $? -eq 1 ]]; then
         filter $1    #不是可执行程序，开始判断是否需要过滤
         if [[ $? -eq 0 ]]; then    #不需要过滤
-        #echo $1"文件不需过滤，开始查询文本中字符串"
         find_words $1    #查询文本中字符串
         fi
     fi
 }
 
 function find_file() {
-    for i in `ls $1`; do
+    for i in `ls -A $1`; do
         dir_or_file="$1/$i"
-        #echo $dir_or_file
         if [[ -d $dir_or_file ]]; then    #忘了加$，卡一万个小时
+            
             find_file ${dir_or_file}
         else
-            #echo $dir_or_file"是文件类型 检查是否可执行以及判断过滤"
             Exec ${dir_or_file}
         fi
     done
 }
-
+hostname=`hostname`
+IFS=$'\n'
 find_file $1
 for i in `seq 0 2`; do
-    echo "${max_str[$i]} ${path[$i]} linenum:${num[$i]} len:${length[$i]}" #字符串 路径 行号 字符串长度
+    echo -e "${length[$i]}:${max_str[$i]}\t$hostname:${path[$i]}\tLine:${num[$i]}" #字符串 路径 行号 字符串长度
 done
+        
